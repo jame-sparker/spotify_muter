6061624ccfbb7d5f2fe19993996766c0d68101fe
James Parker <u5744091@anu.edu.au>
2017-11-11 16:08:21 +1100

    Renamed wrapper and changed installer

--- installer.sh
+++ installer.sh
@@ -2,9 +2,5 @@
 # This installer is meant to be run under sudo
 # Does not install dependencies
 
-SP_MAIN=spotify_main
-
-SP_DIR=$( echo $( which spotify ) | cut -d"/" -f-3 )
-
-mv ${SP_DIR}/spotify ${SP_DIR}/${SP_MAIN} # Rename original file
-mv ./spotify ./muter.py SP_DIR/ # Move muter files 
+MAIN_DIR=/usr/local/bin/
+cp spotify_muter muter.py ${MAIN_DIR}
